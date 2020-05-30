1d91080db049747c3234128a6296e2d8a4a0a83d
amit-shahwal <amitshahwal24@gmail.com>
2020-05-30 23:00:43 +0530

    logo-/

--- viewController.js
+++ viewController.js
@@ -17,91 +17,90 @@ exports.landing = async (req, res) => {
   }
 };
 exports.business = async (req, res) => {
-    try {
-      const data = await superagent.get(
-        "http://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=344dc7ee6cfd4fe88d91a3441a15d765"
-      );
-      const data2 = JSON.parse(data.text);
-      res.status(200).render("business", {
-        title: "BUSINESS",
-        newss: data2.articles,
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  };
+  try {
+    const data = await superagent.get(
+      "http://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=344dc7ee6cfd4fe88d91a3441a15d765"
+    );
+    const data2 = JSON.parse(data.text);
+    res.status(200).render("business", {
+      title: "BUSINESS",
+      newss: data2.articles,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-  exports.entertainment = async (req, res) => {
-    try {
-      const data = await superagent.get(
-        "http://newsapi.org/v2/top-headlines?country=in&category=entertainment&apiKey=344dc7ee6cfd4fe88d91a3441a15d765"
-      );
-      const data2 = JSON.parse(data.text);
-      res.status(200).render("entertainment", {
-        title: "ENTERTAINMENT",
-        newss: data2.articles,
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  };
+exports.entertainment = async (req, res) => {
+  try {
+    const data = await superagent.get(
+      "http://newsapi.org/v2/top-headlines?country=in&category=entertainment&apiKey=344dc7ee6cfd4fe88d91a3441a15d765"
+    );
+    const data2 = JSON.parse(data.text);
+    res.status(200).render("entertainment", {
+      title: "ENTERTAINMENT",
+      newss: data2.articles,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-  exports.sports = async (req, res) => {
-    try {
-      const data = await superagent.get(
-        "http://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=344dc7ee6cfd4fe88d91a3441a15d765"
-      );
-      const data2 = JSON.parse(data.text);
-      res.status(200).render("sports", {
-        title: "SPORTS",
-        newss: data2.articles,
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  };
+exports.sports = async (req, res) => {
+  try {
+    const data = await superagent.get(
+      "http://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=344dc7ee6cfd4fe88d91a3441a15d765"
+    );
+    const data2 = JSON.parse(data.text);
+    res.status(200).render("sports", {
+      title: "SPORTS",
+      newss: data2.articles,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-  exports.science = async (req, res) => {
-    try {
-      const data = await superagent.get(
-        "http://newsapi.org/v2/top-headlines?country=in&category=science&apiKey=344dc7ee6cfd4fe88d91a3441a15d765"
-      );
-      const data2 = JSON.parse(data.text);
-      res.status(200).render("science", {
-        title: "SCIENCE",
-        newss: data2.articles,
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  exports.tech = async (req, res) => {
-    try {
-      const data = await superagent.get(
-        "http://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=344dc7ee6cfd4fe88d91a3441a15d765"
-      );
-      const data2 = JSON.parse(data.text);
-      res.status(200).render("technology", {
-        title: "TECH",
-        newss: data2.articles,
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  };
+exports.science = async (req, res) => {
+  try {
+    const data = await superagent.get(
+      "http://newsapi.org/v2/top-headlines?country=in&category=science&apiKey=344dc7ee6cfd4fe88d91a3441a15d765"
+    );
+    const data2 = JSON.parse(data.text);
+    res.status(200).render("science", {
+      title: "SCIENCE",
+      newss: data2.articles,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+exports.tech = async (req, res) => {
+  try {
+    const data = await superagent.get(
+      "http://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=344dc7ee6cfd4fe88d91a3441a15d765"
+    );
+    const data2 = JSON.parse(data.text);
+    res.status(200).render("technology", {
+      title: "TECH",
+      newss: data2.articles,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-  exports.health = async (req, res) => {
-    try {
-      const data = await superagent.get(
-        "http://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=344dc7ee6cfd4fe88d91a3441a15d765"
-      );
-      const data2 = JSON.parse(data.text);
-      res.status(200).render("health", {
-        title: "HEALTH",
-        newss: data2.articles,
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  
+exports.health = async (req, res) => {
+  try {
+    const data = await superagent.get(
+      "http://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=344dc7ee6cfd4fe88d91a3441a15d765"
+    );
+    const data2 = JSON.parse(data.text);
+    res.status(200).render("health", {
+      title: "HEALTH",
+      newss: data2.articles,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
